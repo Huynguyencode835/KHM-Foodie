1989fe37a7c9a8051b1173b32c17eb58754fe43b
PH31M <ducminh03102005@gmail.com>
2026-07-23 17:48:43 +0700

    QF - 103 them bot xoa Voucher Frontend v1

--- KHMFoodie/app/dao/vouchersDao.py
+++ KHMFoodie/app/dao/vouchersDao.py
@@ -1,20 +1,21 @@
-from app.models.model import Voucher
 from app.extensions import db
+from app.models.model import Voucher
+
 
 class VouchersDao:
     @staticmethod
     def get_all_by_restaurant(restaurant_id):
         return Voucher.query.filter_by(
             restaurant_id=restaurant_id,
-            active = True
+            active=True
         ).order_by(Voucher.created_at.desc()).all()
 
     @staticmethod
     def get_by_id_and_restaurant(voucher_id, restaurant_id):
         return Voucher.query.filter_by(
-            id = voucher_id,
-            restaurant_id = restaurant_id,
-            active = True
+            id=voucher_id,
+            restaurant_id=restaurant_id,
+            active=True
         ).first()
 
     @staticmethod
@@ -24,10 +25,12 @@ class VouchersDao:
         return voucher
 
     @staticmethod
-    def save():
+    def save(voucher):
         db.session.commit()
+        return voucher
 
     @staticmethod
     def soft_delete(voucher):
         voucher.active = False
         db.session.commit()
+        return voucher
