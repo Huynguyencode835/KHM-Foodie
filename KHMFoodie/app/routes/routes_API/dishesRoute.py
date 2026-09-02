f1d75774e528367b7156b829018c9d024a5e1fe2
KhoiLe <tuankhoi000@gmail.com>
2026-09-02 19:07:07 +0700

    QF-148 Xây dựng API gợi ý top món ăn nổi bật cho Homepage

--- KHMFoodie/app/routes/routes_API/dishesRoute.py
+++ KHMFoodie/app/routes/routes_API/dishesRoute.py
@@ -5,7 +5,9 @@ from app.controllers.restaurantMenuController import RestaurantMenuController
 dishes_api = Blueprint("dishes_api", __name__)
 controller = RestaurantMenuController()
 
+dishes_api.add_url_rule("/top-recommendations", view_func=controller.get_top_recommended_dishes, methods=["GET"])
 dishes_api.add_url_rule("/stats", view_func=login_required(controller.get_dishes_stats), methods=["GET"])
 dishes_api.add_url_rule("/", view_func=login_required(controller.create_dishes), methods=["POST"])
 dishes_api.add_url_rule("/<int:dishes_id>", view_func=login_required(controller.delete_dishes), methods=["DELETE"])
 dishes_api.add_url_rule("/", view_func=login_required(controller.change_dishes_status), methods=["PATCH"])
+
